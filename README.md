2858fe46fedc3310d1827da7858c11ab238e600a
proroy1995 <prasenjitkumar1995@gmail.com>
2024-01-18 22:53:40 +0530

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # html
 # expense
 # javascript
+# javascript
---
+++ gitfiles/demo/gitfinal/multipleuserform/form with delete button/editdeleteform/advancedjavascript/code.js
@@ -0,0 +1,27 @@
+4e) const obj= {
+
+  "key1": "value1",
+  
+  "key2" : "value2",
+  
+  "key3" : "value3"
+  
+  }
+  
+  const obj2 = { ...obj}
+  
+  console.log(obj2 === obj)
+
+  What is the output of question 4e?
+
+
+false , because key and values are different
+
+
+true
+
+
+false, because the objects point to different references
+
+
+none of the above
